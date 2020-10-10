5fabcb453b0331e26b160f1e9cf8fb14ca608311
nicodmt <dumont.nicolas.94@gmail.com>
2020-10-11 00:04:07 +0200

    add Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <ul class="nav-links">
-        <li class="products-link">
-          <router-link to="/">Products</router-link>
-        </li>
-        <li class="cart-link">
-          <router-link to="/cart">Cart</router-link>
-        </li>
-        <li class="login-link">
-          <router-link to="/login">Login</router-link>
-          <router-link
-            v-if="authenticated"
-            to="/login"
-            v-on:click.native="logout()"
-            replace
-            >Logout</router-link
-          >
-        </li>
-      </ul>
-    </div>
-    <router-view />
+    <TheNavigation />
   </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      authenticated: false,
-      mockAccount: {
-        username: "admin",
-        password: "admin",
-      },
-    };
-  },
-  mounted() {
-    if (!this.authenticated) {
-      this.$router.replace({ name: "login" });
-    }
-  },
-  methods: {
-    setAuthenticated(status) {
-      this.authenticated = status;
-    },
-    logout() {
-      this.authenticated = false;
-    },
-  },
-};
+import TheNavigation from "@/components/TheNavigation"
+export default{
+  components: {
+    TheNavigation
+  }
+}
 </script>
 
 <style>
@@ -69,41 +30,4 @@ body {
   margin-top: 60px;
 }
 
-#nav {
-  display: flex;
-}
-
-#nav a {
-  color: #000;
-  text-decoration: none;
-  font-weight: bold;
-}
-
-.nav-links {
-  display: flex;
-}
-
-.products-link {
-  padding-right: 20px;
-  list-style: none;
-}
-.products-link:hover {
-  text-decoration: underline;
-}
-
-.cart-link {
-  padding-right: 20px;
-  list-style: none;
-}
-.cart-link:hover {
-  text-decoration: underline;
-}
-
-.login-link {
-  padding-right: 20px;
-  list-style: none;
-}
-.login-link:hover {
-  text-decoration: underline;
-}
 </style>
--- src/views/Secure.vue
+++ src/views/Secure.vue
@@ -1,33 +1,31 @@
 <template>
-    <div id="secure">
-        <h1>Secure Area</h1>
-        <p>
-            Login succesful!
-        </p>
-        <button type="button" v-on:click="logout()">Logout</button>
-    </div>
+  <div id="secure">
+    <h1>Secure Area</h1>
+    <p>Login succesful!</p>
+    <button type="button" v-on:click="logout()">Logout</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Secure',
-        data() {
-            return {};
-        },
-        methods: {
-            logout() {
-                this.$router.replace({ name: "login" });
-                console.log("Logged out");
-            }
-        }
-    }
+export default {
+  name: "Secure",
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      this.$router.replace({ name: "login" });
+      console.log("Logged out");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    #secure {
-        background-color: #FFFFFF;
-        border: 1px solid #CCCCCC;
-        padding: 20px;
-        margin-top: 10px;
-    }
+#secure {
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  padding: 20px;
+  margin-top: 10px;
+}
 </style>
